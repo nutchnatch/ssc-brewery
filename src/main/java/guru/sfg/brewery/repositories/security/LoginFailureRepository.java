bebbffdb5e4101159d494dd8d3464a6105035c3c
nivaldomonteiro <nushnivaldo@gmail.com>
2022-02-03 23:32:22 +0000

    User lockout using spring security events

--- src/main/java/guru/sfg/brewery/repositories/security/LoginFailureRepository.java
+++ src/main/java/guru/sfg/brewery/repositories/security/LoginFailureRepository.java
@@ -1,8 +1,12 @@
 package guru.sfg.brewery.repositories.security;
 
 import guru.sfg.brewery.domain.security.LoginFailure;
+import guru.sfg.brewery.domain.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LoginFailureRepository extends JpaRepository<LoginFailure, Integer> {
+import java.sql.Timestamp;
+import java.util.List;
 
+public interface LoginFailureRepository extends JpaRepository<LoginFailure, Integer> {
+    List<LoginFailure> findAllByUserAndCreatedDateAfter(User user, Timestamp timestamp);
 }
