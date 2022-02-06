2d4588e6217d87cc4dab2b84aa7b9adf0a60f645
nivaldomonteiro <nushnivaldo@gmail.com>
2022-02-06 22:47:40 +0000

    Added 2FA Option in.

--- src/main/java/guru/sfg/brewery/web/controllers/UserController.java
+++ src/main/java/guru/sfg/brewery/web/controllers/UserController.java
@@ -38,14 +38,23 @@ public class UserController {
         return "user/register2fa";
     }
 
-    private User getUser() {
-        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-    }
-
     @PostMapping("")
     public String confirm2fa(@RequestParam Integer verifyCode) {
 
-        //todo - impl
-        return "index";
+        User user = getUser();
+        log.debug("Entered Code is: " + verifyCode);
+        if(googleAuthenticator.authorizeUser(user.getUsername(), verifyCode)) {
+            User savedUser = userRepository.findById(user.getId()).orElseThrow();
+            savedUser.setUserGoogle2fa(true);
+            userRepository.save(savedUser);
+            return "/index";
+        } else {
+            //bad credentials
+            return "user/register2fa";
+        }
+    }
+
+    private User getUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
     }
 }
